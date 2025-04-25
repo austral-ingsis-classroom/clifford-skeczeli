9459dc9bc5731101bed132234c146b774822824d
skeczeli <skeczeli@gmail.com>
2025-04-25 12:52:09 -0300

    Moved commands map from handler into runner.

--- src/test/java/edu/austral/ingsis/FileSystemRunner.java
+++ src/test/java/edu/austral/ingsis/FileSystemRunner.java
@@ -1,12 +1,29 @@
 package edu.austral.ingsis;
 
 import edu.austral.ingsis.clifford.command.CommandHandler;
+import edu.austral.ingsis.clifford.command.factory.CdFactory;
+import edu.austral.ingsis.clifford.command.factory.CommandFactory;
+import edu.austral.ingsis.clifford.command.factory.LsFactory;
+import edu.austral.ingsis.clifford.command.factory.MkdirFactory;
+import edu.austral.ingsis.clifford.command.factory.PwdFactory;
+import edu.austral.ingsis.clifford.command.factory.RmFactory;
+import edu.austral.ingsis.clifford.command.factory.TouchFactory;
 import edu.austral.ingsis.clifford.filesystem.FileSystem;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class FileSystemRunner {
-  private final CommandHandler commandHandler = new CommandHandler();
+  private final Map<String, CommandFactory> commands =
+      Map.of(
+          "cd", new CdFactory(),
+          "ls", new LsFactory(),
+          "mkdir", new MkdirFactory(),
+          "pwd", new PwdFactory(),
+          "rm", new RmFactory(),
+          "touch", new TouchFactory());
+
+  private final CommandHandler commandHandler = new CommandHandler(commands);
   private final FileSystem fileSystem = new FileSystem();
 
   public FileSystem getFileSystem() {
